a1fadcd0fe2a53c7a603cdcdaf7c54a83ea0f09c
freewind <nowindlee@gmail.com>
2019-04-06 16:43:32 +0800

    demo

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-Kotlin Hello World Demo
-=======================
+Kotlin Data Structure Tree Demo
+===============================
+
+最自由的树的特点：
+
+1. 每个节点的子节点个数没有限定：可以为0个、一个或者多个
+2. 子节点的顺序没有限定：所以要用Set
 
 Run `Hello.kt` in your IDE.
--- src/main/kotlin/example/Hello.kt
+++ src/main/kotlin/example/Hello.kt
@@ -1,7 +1,20 @@
 package example
 
-fun main(args: Array<String>) {
-    println(hello("Kotlin"))
-}
+data class TreeNode(val data: String, val children: Set<TreeNode>)
 
-fun hello(name: String): String = "Hello, $name!"
+
+fun main(args: Array<String>) {
+    val tree = TreeNode("aaa", children = setOf(
+            TreeNode("bbb", children = setOf(
+                    TreeNode("ccc", emptySet())
+            )),
+            TreeNode("ddd", children = setOf(
+                    TreeNode("eee", emptySet()),
+                    TreeNode("fff", emptySet()),
+                    TreeNode("ggg", emptySet())
+            )),
+            TreeNode("hhh", children = emptySet())
+    ))
+
+    println(tree)
+}
